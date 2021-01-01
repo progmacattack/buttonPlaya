9dac2addaa6502a10846c45cb26a74fd3e2dd47a
Adam Sickmiller <sickmiller@gmail.com>
2021-01-01 14:38:50 -0500

    Update minimal example.py

--- buttonPlaya/minimal example.py
+++ buttonPlaya/minimal example.py
@@ -1,3 +1,4 @@
+""" This is a minimal example of using the mopidy json rpc post api to plays something on mopidy served at localhost """
 import requests
 
 payload = {"jsonrpc": "2.0", "id": 1, "method": "core.playback.play"}
